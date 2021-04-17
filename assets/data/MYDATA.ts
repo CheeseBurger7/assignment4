9c37d9839f992a443abd4ef5bff7225864bd2c4a
CheeseBurger7 <37046535+CheeseBurger7@users.noreply.github.com>
2021-04-16 21:27:23 -0400

    Add files via upload

---
+++ assets/data/MYDATA.ts
@@ -0,0 +1,9 @@
+import { Rachhadi } from '../../app/rachhadi';
+
+export const MYDATA: Rachhadi = {
+    studentNumber: "991587874",
+    name: "Meet",
+    loginName: "rachhadi",
+    campus: "Davis",
+    assmtTitle: "Assignment-4"
+  }
